bc8c35930b06b3824df0f047fff4a9c86b130a7b
LucasWu91 <lucas.wu@edu.esiee.fr>
2024-09-16 13:07:28 +0000

    First commit

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
+"""
+Fichier main.py
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Détermine si p est un nombre premier ou non
 
-    # votre code ici
+    Args :
+            p : entier positive
+    Returns:
+            True or False
 
-    pass
-
+    """
+    if p<= 1 :
+        return False
+    #si le nombre choisi est inférieure a 1, ce n'est pas un nombre premier ;
+    for i in range(2,int(sqrt(p)+1)):
+        if p%i==0 :
+            return False
+    #Si le nombre est divisible par autre chose, ce n'est pas un nombre premier ;
+    return True
+    #Le nombre est premier ;
 #### Fonction principale
 
 
 def main():
-
+    """
+    Test différentes valeurs
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
